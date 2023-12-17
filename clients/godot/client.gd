5754f1bb1aae30ccc98a9a8a9344b7317778f9fd
Arthur R Longbottom <art.longbottom.jr@gmail.com>
2023-12-16 23:36:12 -0800

    silence errors

--- clients/godot/client.gd
+++ clients/godot/client.gd
@@ -2,11 +2,8 @@ extends Control
 
 @onready var _client: WebSocketClient = $WebSocketClient
 @onready var _log_dest = $Panel/VBoxContainer/RichTextLabel
-@onready var _line_edit = $Panel/VBoxContainer/Send/LineEdit
+#@onready var _line_edit = $Panel/VBoxContainer/Send/LineEdit
 @onready var _host = $Panel/VBoxContainer/Connect/Host
-
-func _ready():
-	pass
 
 func info(msg):
 	print(msg)
@@ -28,13 +25,13 @@ func _on_web_socket_client_message_received(message):
 
 
 # UI signals.
-func _on_send_pressed():
-	if _line_edit.text == "":
-		return
-
-	info("Sending message: %s" % [_line_edit.text])
-	_client.send(_line_edit.text)
-	_line_edit.text = ""
+#func _on_send_pressed():
+	#if _line_edit.text == "":
+		#return
+#
+	#info("Sending message: %s" % [_line_edit.text])
+	#_client.send(_line_edit.text)
+	#_line_edit.text = ""
 
 
 func _on_connect_toggled(pressed):
